7ff66ae202e729be2216d86e44f2b40041701d9a
apple <839864600@qq.com>
2019-01-31 11:22:09 +0800

    fix

--- vuedemo/firstvue01/src/router/index.js
+++ vuedemo/firstvue01/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Tabbar from '@/components/MainTab'
-import WeexTabbar from '@/components/WeexTabbar'
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Tabbar',
       component: Tabbar
-    },
-    {
-      path: '/weextabbar',
-      name: 'WeexTabbar',
-      component: WeexTabbar
     }
   ],
   mode: 'history'
