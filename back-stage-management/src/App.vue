d5fd7279b805cf1af40b84aa53ea4016a64c58ae
luanyoushu <luanyoushu@xiaoheiban.cn>
2019-04-26 19:08:32 +0800

    s

---
+++ back-stage-management/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Main msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Main from './components/Main.vue';
+
+@Component({
+  components: {
+    Main,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 0px;
+}
+</style>
---
+++ back-stage-management/src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div @click="toast('哈哈哈')">
+    haahha ss {{data}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+@Component
+export default class Main extends Vue {
+  data: string = '我的 data'
+  @Prop() private msg!: string;
+  
+  toast(info: string) {
+    this.data = this.data + 1
+  }
+
+  @Watch('data', {deep: true})
+  updataData() {
+    // eslint-disable-next-line
+    console.log(this.data)
+  } 
+
+  mounted () {
+  }
+}
+</script>
