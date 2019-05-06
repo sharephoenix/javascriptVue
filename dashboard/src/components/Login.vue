6e4f674310c9b56e6a45c81e59fe6da0df8f6090
alexluan <326083325@qq.com>
2019-05-06 23:00:22 +0800

    s

---
+++ dashboard/src/components/Login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="body">
+    <div>
+      <div>user</div>
+      <div>secrect</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+  .body {
+    background-color: #f0f0f0;
+  }
+</style>
---
+++ dashboard/src/main.ts
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
+import Login from '@/components/Login.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+Vue.use(VueRouter)
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+//创建路由对象并配置路由规则
+let router = new VueRouter({
+  //routes
+  routes: [
+  //一个个link对象
+  {path: '/login',name: 'app',component: Login},
+  {path: '/',name: 'app',component: HelloWorld}
+]
+});
+
+new Vue({
+  render: h => h(App),
+  router:router
+}).$mount('#app')
